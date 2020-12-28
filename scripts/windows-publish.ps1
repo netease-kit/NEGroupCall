bfecfa9d6226711d19ab9d77d5bc93f353749cca
liangpeng <liangpeng01@corp.netease.com>
2020-12-28 14:11:10 +0800

    提交ci

---
+++ scripts/windows-publish.ps1
@@ -0,0 +1,31 @@
+[CmdletBinding()]
+param (
+    [string] $archiveName, [string] $targetName
+)
+
+
+$scriptDir = $PSScriptRoot
+$currentDir = Get-Location
+Write-Host "currentDir" $currentDir
+Write-Host "scriptDir" $scriptDir
+
+function Main() {
+
+    New-Item -ItemType Directory $archiveName
+    # 拷贝exe
+    Copy-Item bin\$targetName $archiveName\
+ 
+    # 拷贝依赖
+    windeployqt --qmldir . --plugindir $archiveName\plugins --no-translations --compiler-runtime $archiveName\$targetName
+    # 删除不必要的文件
+    $excludeList = @("*.qmlc", "*.ilk", "*.exp", "*.lib", "*.pdb")
+    Remove-Item -Path $archiveName -Include $excludeList -Recurse -Force
+    # 打包zip
+    Compress-Archive -Path $archiveName $archiveName'.zip'
+}
+
+if ($null -eq $archiveName || $null -eq $targetName) {
+    Write-Host "args missing, archiveName is" $archiveName ", targetName is" $targetName
+    return
+}
+Main
