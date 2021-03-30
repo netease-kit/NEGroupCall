b80baf80bff28e804580be5aa685edf17fa6856a
hanyaping <245921027@qq.com>
2021-03-30 11:51:04 +0800

    测试2

--- iOS/NERtcBasicScene-iOS-ObjC/NLiteAVDemo/AppKey.h
+++ iOS/NERtcBasicScene-iOS-ObjC/NLiteAVDemo/AppKey.h
@@ -10,7 +10,7 @@
 #define AppKey_h
 
 /// 线上 IM key
-static NSString * const kAppKey = @"25435234599999";
+static NSString * const kAppKey = @"2543523459999dsgrger9";
 /// 线上 RTC key (同 IM key)
 static NSString * const kNertcAppkey = @"<#请输入rtc appkey#>";
 /// 线上服务器
